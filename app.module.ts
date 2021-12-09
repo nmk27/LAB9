56fc95bd90b18102cd62b4c8631d7d127ad5a5f6
Niran Raj Pradhan <65772698+nmk27@users.noreply.github.com>
2021-12-09 18:18:30 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core'; import {
+BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component'; import {
+CalculatorComponent } from './calculator/calculator.component'; import
+{ CalculatorKeysComponent } from './calculatorkeys/calculatorkeys.component';
+@NgModule({ 
+declarations: [ 
+AppComponent,
+ CalculatorComponent,
+ CalculatorKeysComponent
+ ],
+ imports: [ 
+ BrowserModule
+ ],
+ providers: [],
+ bootstrap: [AppComponent] 
+}) 
+export class AppModule { }
