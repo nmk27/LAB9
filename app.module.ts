84b4dc50d37e836391b5f79d77155128b3ec9f2a
Niran Raj Pradhan <65772698+nmk27@users.noreply.github.com>
2021-12-09 18:19:29 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core'; import {
-BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component'; import {
-CalculatorComponent } from './calculator/calculator.component'; import
-{ CalculatorKeysComponent } from './calculatorkeys/calculatorkeys.component';
+import { NgModule } from '@angular/core'; 
+import {BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component'; 
+import {CalculatorComponent } from './calculator/calculator.component'; 
+import { CalculatorKeysComponent } from './calculatorkeys/calculatorkeys.component';
+
+
+
 @NgModule({ 
 declarations: [ 
 AppComponent,
